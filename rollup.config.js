f4131c9604e9eaef9b93a3a119b6c4054d4fd309
Eugen Reyzenkind <evgeny.reyzenkind@sap.com>
2024-08-24 22:01:27 +0200

    update package json s

--- rollup.config.js
+++ rollup.config.js
@@ -37,42 +37,58 @@ export default {
     input: `${PACKAGE_NAME}/src/index.ts`,
     external: [...Object.keys(pkg.peerDependencies ?? {})],
     output: [
+        // ESM with .mjs extension
         {
+            file: 'lib/library.esm.mjs',
+            format: 'es',
             sourcemap: true,
-            file: "lib/library.esm.js",
-            format: 'esm'
         },
         {
+            file: 'lib/library.esm.min.mjs',
+            format: 'es',
+            plugins: [terser()],
             sourcemap: true,
-            file: "lib/library.esm.min.js",
-            format: 'esm',
-            plugins: [terser()]
         },
+        // CommonJS with .cjs extension
         {
-            sourcemap: true,
-            file: "lib/library.umd.js",
-            format: 'umd',
-            name: `@semaver/${pkg.rawName}`
-        },
-        {
-            sourcemap: true,
-            file: "lib/library.umd.min.js",
-            format: 'umd',
-            name: `@semaver/${pkg.rawName}`,
-            plugins: [terser()]
-        },
-        {
-            sourcemap: true,
-            file: "lib/library.cjs.cjs",
-            format: 'cjs'
-        },
-        {
-            sourcemap: true,
-            file: "lib/library.cjs.min.cjs",
+            file: 'lib/library.cjs.cjs',
             format: 'cjs',
-            plugins: [terser()]
-        }
-
+            sourcemap: true,
+        },
+        {
+            file: 'lib/library.cjs.min.cjs',
+            format: 'cjs',
+            plugins: [terser()],
+            sourcemap: true,
+        },
+        // UMD with standard .js extension
+        {
+            file: 'lib/library.umd.js',
+            format: 'umd',
+            name: `${pkg.rawName}`,
+            sourcemap: true,
+        },
+        {
+            file: 'lib/library.umd.min.js',
+            format: 'umd',
+            name: `${pkg.rawName}`,
+            plugins: [terser()],
+            sourcemap: true,
+        },
+        // IIFE with standard .js extension
+        {
+            file: 'lib/library.iife.js',
+            format: 'iife',
+            name: `${pkg.rawName}`,
+            sourcemap: true,
+        },
+        {
+            file: 'lib/library.iife.min.js',
+            format: 'iife',
+            name: `${pkg.rawName}`,
+            plugins: [terser()],
+            sourcemap: true,
+        },
     ],
     plugins: [
         cleandir(`${PACKAGE_NAME}/lib`),
